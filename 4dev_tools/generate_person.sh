afd77a154d9a8508e4547b5dc06e8347ebf1f637
robsonandradev <robson.andrade@amicci.com.br>
2025-02-22 22:58:30 -0300

    Format query string

--- 4dev_tools/generate_person.sh
+++ 4dev_tools/generate_person.sh
@@ -7,5 +7,12 @@ estado=PR
 quantidade=5
 cidade=6015
 
-curl -sd "acao=$acao&sexo=I&pontuacao=$pontuacao&idade=$idade&cep_estado=$estado&txt_qtde=$quantidade&cep_cidade=$cidade" \
-https://www.4devs.com.br/ferramentas_online.php | jq
+query_string="acao=$acao"
+query_string="$query_string&sexo=I"
+query_string="$query_string&pontuacao=$pontuacao"
+query_string="$query_string&idade=$idade"
+query_string="$query_string&cep_estado=$estado"
+query_string="$query_string&txt_qtde=$quantidade"
+query_string="$query_string&cep_cidade=$cidade"
+
+curl -sd "$query_string" https://www.4devs.com.br/ferramentas_online.php | jq
